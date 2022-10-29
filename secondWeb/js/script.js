5004f7f4873566d22b5eb56e3f8fa6c99805b3e7
krkzmain <kjsa777@gmail.com>
2022-10-29 17:46:29 +0900

    add type action

---
+++ secondWeb/js/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".typing", {
+    strings:["   ","DotNet","DotNet Developer"],
+    typeSpeed: 100,
+    startDelay: 300,
+    BackSpeed: 60,
+    backDelay: 300,
+    loop:true
+})
