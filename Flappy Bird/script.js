d9290ffe5525023367ac19d1345881cf5033d854
krkzmain <kjsa777@gmail.com>
2022-11-12 16:46:41 +0900

    make component

--- Flappy Bird/script.js
+++ Flappy Bird/script.js
@@ -1,8 +1,4 @@
-function startGame()
-{
-    myGameArea.start();
-}
-
+let myGamePiece;
 const myGameArea = 
 {
     canvas : document.createElement("canvas"),
@@ -11,5 +7,37 @@ const myGameArea =
         this.canvas.height=270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function(){
+        this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
     }
+}
+
+function startGame()
+{
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "blue", 10, 120);
+}
+
+
+function component(width, height, color, x, y)
+{
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.update = function()
+    {
+        ctx =myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea()
+{
+    myGameArea.clear();
+    myGamePiece.x += 1;
+    myGamePiece.update();
 }
