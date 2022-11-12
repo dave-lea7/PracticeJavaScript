25eee15e77bfc3cdc343f17cf9fbdcfe83aeb29f
krkzmain <kjsa777@gmail.com>
2022-11-12 17:58:10 +0900

    error F5 key
    need prevent F5

--- Flappy Bird/script.js
+++ Flappy Bird/script.js
@@ -1,4 +1,6 @@
 let myGamePiece;
+let myObstacle;
+
 const myGameArea = 
 {
     canvas : document.createElement("canvas"),
@@ -9,22 +11,37 @@ const myGameArea =
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (e) {
+            let key = e.key || e.keyCode;
             myGameArea.keys = (myGameArea.keys || []);
-            myGameArea.keys[e.keyCode] = true;
+            console.log(myGameArea.keys);
+            myGameArea.keys[key] = true;
         })
         window.addEventListener('keyup', function (e) {
-            myGameArea.keys[e.keyCode] = false;
+            let key = e.key || e.keyCode;
+            if(key === 'F5')
+            {
+                    console.log(key);
+                    this.start;         
+            }else
+            {
+                myGameArea.keys[key] = false;
+            }
+            
         })
     },
     clear : function(){
         this.context.clearRect(0,0, this.canvas.width, this.canvas.height);
+    },
+    over : function() {
+        clearInterval(this.interval);
     }
 }
 
 function startGame()
 {
     myGameArea.start();
-    myGamePiece = new component(30, 30, "blue", 10, 120);
+    myGamePiece = new component(30, 30, "pink", 10, 120);
+    myObstacle = new component(10, 200, "green", 300, 120);
 }
 
 
@@ -47,11 +64,30 @@ function component(width, height, color, x, y)
         this.x += this.speedX;
         this.y += this.speedY;
     }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = true;
+        if ((mybottom < othertop) ||
+        (mytop > otherbottom) ||
+        (myright < otherleft) ||
+        (myleft > otherright)) {
+          crash = false;
+        }
+        return crash;
+      }
 }
 
 function updateGameArea()
 {
     myGameArea.clear();
+    myObstacle.update();
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
     if (myGameArea.keys && myGameArea.keys[37]) {myGamePiece.speedX = -1; }
@@ -59,6 +95,7 @@ function updateGameArea()
     if (myGameArea.keys && myGameArea.keys[38]) {myGamePiece.speedY = -1; }
     if (myGameArea.keys && myGameArea.keys[40]) {myGamePiece.speedY = 1; }
     myGamePiece.newPos();
+    
     myGamePiece.update();
 }
 
