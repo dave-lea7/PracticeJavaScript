ac6010f9c7a27ffe8ce617a82fc4e0f0841565e6
krkzmain <kjsa777@gmail.com>
2022-12-13 14:40:12 +0900

    modify div

--- RandomPage/makecalendar.js
+++ RandomPage/makecalendar.js
@@ -4,7 +4,7 @@ console.log(new Date('2022-12-09T03:24:00'));
 
 let date = new Date();
 let utcDate = date.getTime() + (date.getTimezoneOffset() * 60 * 1000);
-let kstGap = 9*60*60*1000; //9시간 차이임
+let kstGap = 9 * 60 * 60 * 1000; //9시간 차이임
 let today = new Date(utcDate + kstGap);
 
 const nowDate = new Date(today);
@@ -21,9 +21,35 @@ console.log(startDate);
 console.log(prevDate);
 console.log(prevDay);
 // 이번 달의 마지막 날
-const endDate = new Date(currentYear, currentMonth +1 ,0);
+const endDate = new Date(currentYear, currentMonth + 1, 0);
 const nextDate = endDate.getDate();
 const nextDay = endDate.getDay();
 console.log(endDate);
 console.log(nextDate);
 console.log(nextDay);
+
+//이전
+function next(){
+
+}
+//다음
+function previous(){
+    
+}
+
+//점프
+function jump() {
+    currentYear = parseInt(selectYear.value);
+    currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+}
+
+//달력 쇼
+function showCalendar(month,year){
+
+}
+
+//날짜
+function daysInMonth(iMonth,iYear){
+
+}
